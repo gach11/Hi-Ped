c14d85a8524f59d5bf1dfe7cf560537b9597877b
EmmanuelCara <EmmanuelCara@G07PHXNWES03635.g07.fujitsu.local>
2022-03-02 11:21:25 +0800

    send email fixed

---
+++ hi_ped_website/src/main/java/sendEmail/sendEmail.java
@@ -0,0 +1,64 @@
+package sendEmail;
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/sendEmail")
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2,   // 2MB
+maxFileSize = 1024 * 1024 * 10,         // 10MB
+maxRequestSize = 1024 * 1024 * 50)      // 50MB
+public class sendEmail extends HttpServlet {
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String host;
+	private String port;
+	private String user;
+	private String pass;
+
+	public void init() {
+		// reads SMTP server setting from web.xml file
+		ServletContext context = getServletContext();
+		host = context.getInitParameter("host");
+		port = context.getInitParameter("port");
+		user = context.getInitParameter("user");
+		pass = context.getInitParameter("pass");
+}
+
+/**
+* handles form submission
+*/
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		String recipient = request.getParameter("recipient");
+		String subject = request.getParameter("subject");
+		String content = request.getParameter("content");
+
+		String resultMessage = "";
+
+		try {
+			emailUtility.sendEmail(host, port, user, pass,
+					recipient, subject, content);
+
+			resultMessage = "The e-mail was sent successfully";
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			resultMessage = "There were an error: " + ex.getMessage();
+		} finally {
+			request.setAttribute("message", resultMessage);
+			getServletContext().getRequestDispatcher("/secretary_dashboard.jsp").forward(
+					request, response);
+		}
+	}
+}
